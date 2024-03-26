898c11758921032664c620482396fccec8b7e3fe
Chirag Mehta <chiragmehta1010@gmail.com>
2024-03-27 00:43:27 +0530

    Fixed Issue #2
    Removed an extraneous print statement, and fixed issue #2

--- preprocessing.py
+++ preprocessing.py
@@ -27,9 +27,26 @@ def get_intervals(df: pd.DataFrame, interval_duration: float = 1):
     target_attr = 'Sample (V)'
     delta = df.iloc[1][time_attr] - df.iloc[0][time_attr]
     interval_length = int(60_000 * interval_duration / delta)
-    print(interval_length)
     arr = []
-    for i in range(0, df.shape[0] - interval_length + 1, interval_length // 2):
+
+    epsilon = 1e-1
+    num_intervals = int(df.shape[0] / interval_length + epsilon)
+
+    # cramp is the additional length that needs to be
+    # compensated for, i.e., even the alternate intervals
+    # end up having some overlap
+    cramp = num_intervals * interval_length - len(df)
+    # if the size of interval is slightly bigger then the
+    # overlap between consecutive intervals would
+    # be a little less than 50%
+    cramp = max(0, cramp)
+
+    interval_indices = np.linspace(0,
+                                   len(df) - cramp,
+                                   num=2 * num_intervals,
+                                   endpoint=False)[:-1]
+    for i in interval_indices:
+        i = int(i)
         interval = df[target_attr].iloc[i:i + interval_length].to_numpy()
         arr.append(interval)
     arr = np.array(arr)
